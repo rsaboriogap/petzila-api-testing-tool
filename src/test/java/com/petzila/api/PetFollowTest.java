407be800befe105864855397ada335a66e87e58a
vicente <vsansilvestregap@wearegap.com>
2015-01-19 17:00:17 -0600

    adding logic PetUnFollow tests

--- src/test/java/com/petzila/api/PetFollowTest.java
+++ src/test/java/com/petzila/api/PetFollowTest.java
@@ -2,10 +2,7 @@ package com.petzila.api;
 
 import com.petzila.api.model.Pet;
 import com.petzila.api.model.Post;
-import com.petzila.api.model.response.PetCreateResponse;
-import com.petzila.api.model.response.PetFollowUnfollowResponse;
-import com.petzila.api.model.response.PetGetResponse;
-import com.petzila.api.model.response.PostCreateResponse;
+import com.petzila.api.model.response.*;
 import com.petzila.api.util.Users;
 import com.petzila.api.util.Utils;
 import org.junit.Before;
@@ -42,14 +39,28 @@ public class PetFollowTest {
         pet.resourceType = "image/jpeg";
         petId = Petzila.PetAPI.create(pet, userKey).data.id;
 
+    }
 
+    @Before
+    public void beforeEachTest() throws Exception {
+        PetFollowUnfollowResponse response =Petzila.PetAPI.unfollow(petId, userKey2);
+        assertNotNull(response);
+        assertEquals(response.status, "Success");
+        assertTrue(response.data.followersCounter >=0);
     }
 
     @Test
     public void testFollowHappyPath() throws Exception {
+        PetFollowersResponse petFollowersResponseBefore = Petzila.PetAPI.getFollowers(petId);
+
         PetFollowUnfollowResponse response =Petzila.PetAPI.follow(petId,userKey2);
         assertNotNull(response);
         assertEquals(response.status, "Success");
+        assertTrue(response.data.followersCounter >=0);
+
+        PetFollowersResponse petFollowersResponseAfter = Petzila.PetAPI.getFollowers(petId);
+        assertTrue(petFollowersResponseBefore.data.length+1 == petFollowersResponseAfter.data.length);
+
     }
 
     @Test
@@ -57,27 +68,29 @@ public class PetFollowTest {
         PetFollowUnfollowResponse response =Petzila.PetAPI.follow(petId,userKey2);
         assertNotNull(response);
         assertEquals(response.status, "Success");
-        System.out.println();
+
+        int followersCounter= response.data.followersCounter;
         assertTrue(response.data.followersCounter >=0);
+
 
         PetFollowUnfollowResponse response2 =Petzila.PetAPI.follow(petId,userKey2);
         assertNotNull(response2);
         assertEquals(response2.status, "Success");
-        assertTrue(response2.data.followersCounter >= 0);
+        assertTrue(response2.data.followersCounter == followersCounter);
 
         PetFollowUnfollowResponse response3 =Petzila.PetAPI.follow(petId,userKey2);
         assertNotNull(response3);
         assertEquals(response3.status, "Success");
-        assertTrue(response3.data.followersCounter >= 0);
+        assertTrue(response3.data.followersCounter == followersCounter);
 
         PetFollowUnfollowResponse response4 =Petzila.PetAPI.follow(petId,userKey2);
         assertNotNull(response4);
         assertEquals(response4.status, "Success");
-        assertTrue(response4.data.followersCounter >= 0);
+        assertTrue(response4.data.followersCounter == followersCounter);
 
         PetFollowUnfollowResponse response5 =Petzila.PetAPI.follow(petId,userKey2);
         assertNotNull(response5);
         assertEquals(response5.status, "Success");
-        assertTrue(response5.data.followersCounter >= 0);
+        assertTrue(response5.data.followersCounter == followersCounter);
     }
 }
--- src/test/java/com/petzila/api/PetUnfollowTest.java
+++ src/test/java/com/petzila/api/PetUnfollowTest.java
@@ -2,8 +2,10 @@ package com.petzila.api;
 
 import com.petzila.api.model.Pet;
 import com.petzila.api.model.response.PetFollowUnfollowResponse;
+import com.petzila.api.model.response.PetFollowersResponse;
 import com.petzila.api.util.Users;
 import com.petzila.api.util.Utils;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -35,7 +37,10 @@ public class PetUnfollowTest {
         pet.profilePicture = Utils.asBase64("/dog1.jpg");
         pet.resourceType = "image/jpeg";
         petId = Petzila.PetAPI.create(pet, userKey).data.id;
+    }
 
+    @Before
+    public void beforeEachTest()throws Exception{
         PetFollowUnfollowResponse response =Petzila.PetAPI.follow(petId,userKey2);
         assertNotNull(response);
         assertEquals(response.status, "Success");
@@ -43,33 +48,39 @@ public class PetUnfollowTest {
 
     @Test
     public void testUnFollowHappyPath() throws Exception {
+        PetFollowersResponse petFollowersResponseBefore = Petzila.PetAPI.getFollowers(petId);
+
         PetFollowUnfollowResponse response =Petzila.PetAPI.unfollow(petId, userKey2);
         assertNotNull(response);
         assertEquals(response.status, "Success");
         assertTrue(response.data.followersCounter >=0);
+
+        PetFollowersResponse petFollowersResponseAfter= Petzila.PetAPI.getFollowers(petId);
+        assertTrue(petFollowersResponseBefore.data.length-1 == petFollowersResponseAfter.data.length);
     }
 
     @Test
     public void testUnFollowConcurrencyIntegrityTest() throws Exception {
-        PetFollowUnfollowResponse responseFollow =Petzila.PetAPI.follow(petId,userKey2);
-        assertNotNull(responseFollow);
-        assertEquals(responseFollow.status, "Success");
-        System.out.println();
-        assertTrue(responseFollow.data.followersCounter >=0);
+        PetFollowersResponse petFollowersResponseBefore = Petzila.PetAPI.getFollowers(petId);
+        int followersCounter=petFollowersResponseBefore.data.length ;
 
         PetFollowUnfollowResponse responseUnFollow =Petzila.PetAPI.unfollow(petId, userKey2);
         assertNotNull(responseUnFollow);
         assertEquals(responseUnFollow.status, "Success");
+
         assertTrue(responseUnFollow.data.followersCounter >= 0);
+        assertTrue(followersCounter-1==responseUnFollow.data.followersCounter);
 
         PetFollowUnfollowResponse responseUnFollow2 =Petzila.PetAPI.unfollow(petId, userKey2);
         assertNotNull(responseUnFollow2);
         assertEquals(responseUnFollow2.status, "Success");
         assertTrue(responseUnFollow2.data.followersCounter >= 0);
+        assertTrue(followersCounter-1==responseUnFollow2.data.followersCounter);
 
         PetFollowUnfollowResponse responseUnFollow3 =Petzila.PetAPI.unfollow(petId, userKey2);
         assertNotNull(responseUnFollow3);
         assertEquals(responseUnFollow3.status, "Success");
         assertTrue(responseUnFollow3.data.followersCounter >= 0);
+        assertTrue(followersCounter-1==responseUnFollow3.data.followersCounter);
     }
 }
